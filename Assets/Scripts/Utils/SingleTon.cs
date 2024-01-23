21f80b039cd6acda49d0505edbfcb372bc49eb2c
Yunho <bigunit3030@gmail.com>
2024-01-23 13:24:45 +0900

    feat : SingleTon 추가

---
+++ Assets/Scripts/Utils/SingleTon.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleTon<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public const string gameObjectName = "Manager";
+
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (null == instance)
+            {
+                instance = FindObjectOfType<T>();
+                if (null == instance)
+                {
+                    GameObject gameObject = new GameObject();
+                    gameObject.name = gameObjectName;
+                    instance = gameObject.AddComponent<T>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
