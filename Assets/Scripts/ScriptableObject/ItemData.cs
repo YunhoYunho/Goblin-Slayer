ff7d69dc33be59a8968611d2e15374ec5146443c
Yunho <bigunit3030@gmail.com>
2024-01-30 16:26:27 +0900

    feat : ItemData 추가

---
+++ Assets/Scripts/ScriptableObject/ItemData.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    Weapon,
+    Consumable,
+
+    Size,
+}
+
+[CreateAssetMenu(menuName = "Scriptable/Item")]
+public class ItemData : ScriptableObject
+{
+    [Header("Common")]
+    public ItemType type;
+    public string itemName;
+    public Sprite image;
+    public bool isStackable = true;
+    public int itemCost;
+    public float itemAbilityValue;
+}
